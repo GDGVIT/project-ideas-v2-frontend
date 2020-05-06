26399e7953b0d7b095296e89f40569164dc9dd73
ashutosh.k <ashutosh.k@inqutec.com>
2020-05-06 14:58:51 +0530

    user profile added

---
+++ src/edit.js
@@ -0,0 +1,163 @@
+import React, {useState, useEffect} from 'react'
+import {Form, Input, Button, Tag} from 'antd'
+import { withAlert } from 'react-alert'
+import Load2 from './loading2' 
+// import {v4 as uuidv4} from 'uuid'
+// import {WithOutContext as ReactTags} from 'react-tag-input'
+
+const Editidea = (props) =>{
+
+    const [loading, setLoad] = useState(false)
+    const [al, setAl] = useState(true)
+    const [tagz, setTags] = useState(["Sample"])
+    const [val, setVal] = useState('')
+    const [title, setTitle] = useState('')
+    const [desc, setDesc] = useState('')
+
+
+        useEffect(() => {
+            console.log(props)
+            setTitle(props.ideaData.project_title)
+            setDesc(props.ideaData.project_description)
+            setTags(props.ideaData.tags.split(','))
+        }, [])
+
+    const handleDelete = (i) => {
+        // i.preventDefault()
+        console.log(i)
+        setTags(
+            tagz.filter((tag, index) => index !== i)
+        )
+        console.log(tagz)
+
+    }
+ 
+   const handleAddition = () => {
+       if(!tagz.includes(val.trim())){
+        setTags([...tagz, val.trim()]);
+        setVal('')
+       }else{
+        setVal('')
+       }
+    }
+    const handleSpaceAddition = (e) =>{
+        if(e.key===' '){
+            if(!tagz.includes(val.trim())){
+                setTags([...tagz, val.trim()]);
+                setVal('')
+               }else{
+                setVal('')
+               }
+        }
+    }
+
+
+    const onFinish =()=>{
+        let tagz_String = '' 
+        tagz.forEach(element => {
+            tagz_String += (element + ',')     
+        });
+        let values = {
+            id: props.ideaData.id,
+            project_title: title,
+            project_description: desc,
+            tags: tagz_String
+        }
+        setLoad(true)
+        console.log(values)
+        if(values.project_title==='' || values.project_description==='' || values.tags===''){
+            props.alert.show('You can not leave any of the fields empty!')
+            setLoad(false)
+        }else{
+            fetch(process.env.REACT_APP_BASEURL+'app/user_idea/', {
+                method: 'PUT',
+                headers: new Headers({
+                    'Content-Type': 'application/json',
+                    'Authorization': localStorage.getItem('token')
+                }),
+                body:JSON.stringify(values)
+            })
+            .then(res=>{
+                if(res.status === 200){
+                    setLoad(false)
+                    props.alert.show("Idea updated")
+                    props.closeThis()
+                }else if(res.status===403){
+                    setLoad(false)
+                        if(al){
+                            props.alert.show('You need to log in to perform this action')
+                            setAl(false)
+                            setTimeout(()=>{
+                                setAl(true)
+                            },5000)
+                            }
+                }    
+                return(res.json())
+            })
+            .then(data=>{
+                // console.log(data)
+                setLoad(false)
+            })
+            .catch(error=>{
+                props.alert.show('something went wrong')
+                setLoad(false)
+            })
+        }
+
+    }
+
+    // const handleInputChange = (v, type) =>{
+    //     v.persist()
+    //     console.log(v.target.value)
+    //     if(v.target.value.length){
+    //         if(type === "title"){
+    //             setTitle(v.target.value.trim())
+    //         }else{
+    //             setDesc(v.target.value.trim())
+    //         }
+    //     }
+    // }
+    return(
+        <div className='formholder'>
+        {loading && <Load2 />}
+        <h2>Edit Idea</h2>
+            <Form name="IdeaForm">
+                <h2>Title</h2>
+                <Form.Item name='project_title' rules={[
+                    { required: true, message: 'You can not leave this empty!' },
+                        {max: 100, message:'max 100 characters only!'}
+
+                    ]}>
+                    <Input defaultValue={props.ideaData.project_title} onChange={(v)=>{setTitle(v.target.value.trim())}}/>
+                </Form.Item>
+                <h2>Description</h2>
+                <Form.Item name='project_description' 
+                rules={[
+                    { required: true, message: 'You can not leave this empty!' },
+                    {max: 500, message:'max 500 characters only!'}
+                    ]}
+                >
+                    <Input.TextArea onChange={(v)=>{setDesc(v.target.value.trim())}} defaultValue={props.ideaData.project_description} />
+                </Form.Item>
+                <h2>Tags</h2>
+                <Form.Item name='tags' 
+                rules={[
+                    { required: true, message: 'You can not leave this epmty!' },
+                    {max: 50, message:'max 50 characters only!'}
+                    ]}
+                    >
+                    <Input placeholder='Mention tags for your project'  value={val} onChange={(val)=>setVal(val.target.value)} onPressEnter={handleAddition} onKeyPress={handleSpaceAddition}/>
+                    {tagz.map((tag, index)=>{
+                        return(<Tag color="blue" key={tag} closable onClose={()=>{handleDelete(index)}}> {tag} </Tag>)
+                    })}
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type='primary' onClick={onFinish}>Submit</Button>
+                </Form.Item>
+            </Form>
+        </div>
+    )
+}
+
+export default withAlert()(Editidea);
---
+++ src/profile.js
@@ -0,0 +1,162 @@
+import React from 'react'
+import {message, Card, Drawer} from 'antd'
+import {withRouter} from 'react-router-dom'
+import Editidea from './edit'
+
+class Profile extends React.Component{
+    state={
+        ideas: [],
+        loading: false,
+        anyIdea: false,
+        mssg: '',
+        visibleEdit: false,
+        lastEditIdea : {}
+    }
+
+    componentDidMount(){
+        fetch(process.env.REACT_APP_BASEURL+'app/user_idea/', {
+            method:'GET',
+            headers: new Headers({
+                'Authorization':localStorage.getItem('token')
+            })
+          })
+          .then(res=>res.json())
+          .then(data=>{
+                console.log(data)
+                this.setState({
+                ideas:data.message,
+                })
+          })
+          .catch(error=>{
+            if(error){
+              console.log(error)
+              this.setState({
+                mssg:"Seems like you haven't posted any ideas yet",
+              })
+            }
+          })
+    }
+    onClose=()=>{
+        this.setState({
+            visibleEdit:false
+        })
+    }
+    logout=()=>{
+        fetch(process.env.REACT_APP_BASEURL+'app/logout/', {
+            headers: new Headers({
+                'Authorization': localStorage.getItem("token")
+                }),
+        }).then(response => response.json())
+        .then(data=>{
+            localStorage.removeItem("token");
+            this.setState({isLoggedIn:false})
+        })
+        .catch(error=>{
+            if(error){
+                console.error(error);
+            }
+        })
+      }
+      openSidea=(id)=>{
+        this.props.history.push('/ideas/'+id)
+      }
+
+      deleteIdea=(id)=>{
+        fetch(process.env.REACT_APP_BASEURL+'app/user_idea/'+id+'/',{
+            method:'DELETE',
+            headers: new Headers({
+                Authorization: localStorage.getItem('token')
+            })
+          })
+          .then(res=>res.json())
+          .then(data=>{
+              console.log(data)
+            let ideaList = this.state.ideas.filter(x => {
+                return x.id !== id;
+              })
+              this.setState({
+                ideas: ideaList
+              })
+              message.info('Idea deleted')
+          })
+          .catch(error=>{
+            if(error){
+            //   this.props.history.push('/ideas')
+            console.log(error)
+            }
+          })
+
+      }
+
+      openEdit=(id)=>{
+        fetch(process.env.REACT_APP_BASEURL+'app/view_idea/'+id+'/',{
+            method:'GET'
+          })
+          .then(res=>res.json())
+          .then(data=>{
+            this.setState({
+                lastEditIdea:data.message,
+                visibleEdit: true
+            })
+          })
+          .catch(error=>{
+            if(error){
+            //   this.props.history.push('/ideas')
+            console.log(error)
+            }
+          })
+      }
+
+    render(){
+        const {ideas, mssg}=this.state
+        var dataDisp = ideas.length?(
+          ideas.map(data=>{
+            let desc=''
+            if(data.project_description.length>=170){
+              desc = data.project_description.substring(0,170)+'...'
+            }else{
+              desc = data.project_description;
+            }
+            return(
+              <Card key={data.id}>
+              <h3>
+                {data.project_title}
+              </h3>
+              <p className="idea-desc">{desc}</p>
+              <p style={{color:"#2785FC", marginTop:'20px'}}>
+                {data.votes} Votes
+              </p>
+              <div className="profile-card-buttonholder">
+                <button onClick={()=>{this.deleteIdea(data.id)}}>Delete</button>
+                <button onClick={()=>{this.openEdit(data.id)}}>Edit</button>
+                <button  onClick={()=>{this.openSidea(data.id)}}>View</button>
+              </div>
+            </Card>
+            )
+          })
+        ):(<div style={{textAlign:"center"}}>{mssg}</div>)
+        return(
+            <div className="profile-par">
+                <div className="logout-button">
+                    <button onClick={this.logout}>Logout</button>
+                </div>
+                <div className="profile-ideas">
+                    <h2>Your ideas</h2>
+                    {dataDisp}
+                </div>
+                <Drawer
+                        placement="right"
+                        closable={true}
+                        onClose={this.onClose}
+                        visible={this.state.visibleEdit}
+                        width={window.innerWidth<400?(window.innerWidth):(400)}
+                        zIndex="2001"
+                    >
+                        <Editidea closeThis={this.onClose} ideaData={this.state.lastEditIdea}/>
+                </Drawer>
+            </div>
+        )
+    }
+}
+
+export default withRouter(Profile)
